a3c8eae46794eb7c82ac7137d46e17a86ad641f2
null-kryptonian <57069206+null-kryptonian@users.noreply.github.com>
2020-06-18 18:18:56 +0600

    Create 0041a.cpp

---
+++ Codeforces/0041a.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    string s, t;
+    cin >> s >> t;
+    reverse(t.begin(), t.end());
+
+    if(s == t) {
+        cout << "YES" << endl;
+    }
+    else {
+        cout << "NO" << endl;
+    }
+}
