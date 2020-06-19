c4ce3e9d94d6b82de25ad0b1d756899de3fb2080
null-kryptonian <57069206+null-kryptonian@users.noreply.github.com>
2020-06-19 13:03:09 +0600

    Create 0318a.cpp

---
+++ Codeforces/0318a.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+    ll a, b;
+    cin >> a >> b;
+
+    if(b <= (a+1) / 2) {
+        cout << 2 * b - 1 << endl;
+    }
+    else {
+        cout << (b - (a+1) / 2) * 2 << endl;
+    }
+}
+
