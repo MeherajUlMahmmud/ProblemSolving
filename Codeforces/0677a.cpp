b2f8faf198482a911e3f1754314f34616ccf8154
null-kryptonian <57069206+null-kryptonian@users.noreply.github.com>
2020-06-19 20:46:45 +0600

    Create 0677a.cpp

---
+++ Codeforces/0677a.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n, h, w = 0;
+    cin >> n >> h;
+    int arr[n];
+    for(int i = 0; i < n; ++i) {
+        cin >> arr[i];
+        if(arr[i] > h)
+            w += 2;
+        else
+            w++;
+    }
+    cout << w;
+
+
+}
