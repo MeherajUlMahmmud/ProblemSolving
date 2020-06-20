197deadc6b24f09033ad4ae0ba4e02161bc55e16
null-kryptonian <57069206+null-kryptonian@users.noreply.github.com>
2020-06-20 21:18:05 +0600

    Create 0025a.cpp

---
+++ Codeforces/0025a.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n, even = 0, odd = 0;
+    cin >> n;
+
+    int arr[n];
+    for(int i = 0; i < n; ++i)
+    {
+        cin >> arr[i];
+        if(arr[i] % 2 == 0)
+        {
+            even++;
+        }
+        else
+        {
+            odd++;
+        }
+    }
+
+    if(even > odd)
+    {
+        for(int i = 0; i < n; ++i)
+        {
+            if(arr[i] % 2 != 0)
+            {
+                cout << i+1;
+                break;
+            }
+        }
+    }
+    else
+    {
+        for(int i = 0; i < n; ++i)
+        {
+            if(arr[i] % 2 == 0)
+            {
+                cout << i+1;
+                break;
+            }
+        }
+    }
+}
