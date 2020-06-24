cdb350d025ea3bc229ea0ea21afcb595ac6aac58
null-kryptonian <57069206+null-kryptonian@users.noreply.github.com>
2020-06-24 13:36:48 +0600

    Create 35. Search Insert Position (Naive).cpp

---
+++ LeetCode/35. Search Insert Position (Naive).cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int i = 0;
+        for(i = 0; i < nums.size(); ++i) {
+            if(nums[i] == target)
+                return i;
+            else if(nums[i] > target)
+                return i;
+        }
+        return i++;
+    }
+};
