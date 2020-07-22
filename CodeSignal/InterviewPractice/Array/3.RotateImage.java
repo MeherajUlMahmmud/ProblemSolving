0367fde505c30ef2e4c282ceb75e743e75139a03
null-kryptonian <57069206+null-kryptonian@users.noreply.github.com>
2020-07-22 17:59:11 +0600

    Create 3.RotateImage.java

---
+++ CodeSignal/InterviewPractice/Array/3.RotateImage.java
@@ -0,0 +1,20 @@
+int[][] rotateImage(int[][] arr) {
+    int n = arr.length;
+    for(int i = 0; i < n; i++) {
+        for(int j = i; j < n; j++) {
+            int temp = arr[i][j];
+            arr[i][j] = arr[j][i];
+            arr[j][i] = temp;
+        }
+    }
+    
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n/2; j++) {
+            int temp = arr[i][j];
+            arr[i][j] = arr[i][n-1-j];
+            arr[i][n-1-j] = temp;
+        }
+    }
+    
+    return arr;
+}
