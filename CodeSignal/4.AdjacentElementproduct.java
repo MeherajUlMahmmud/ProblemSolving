187991decf0b87b88bd174a2308025346f94e158
null-kryptonian <57069206+null-kryptonian@users.noreply.github.com>
2020-07-22 17:58:16 +0600

    Create 4.AdjacentElementproduct.java

---
+++ CodeSignal/4.AdjacentElementproduct.java
@@ -0,0 +1,11 @@
+int adjacentElementsProduct(int[] arr) {
+    int result = -9999;
+    
+    for(int i = 0; i < arr.length-1; i++) {
+        int product = arr[i]*arr[i+1];
+        if(product > result)
+            result = product;
+    }
+    
+    return result;
+}
