924953c905798b6c68486d487719a7641d4d9fb7
null-kryptonian <57069206+null-kryptonian@users.noreply.github.com>
2020-07-29 20:32:58 +0600

    Update 53. Maximum Subarray.cpp

--- LeetCode/53. Maximum Subarray.cpp
+++ LeetCode/53. Maximum Subarray.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
         int maxSum = nums[0], curSum = 0;
-        for(int i : nums) {
+        for(int i = 0; i < nums.size(); i++) {
             if(curSum < 0)
                 curSum = 0;
             curSum += i;
