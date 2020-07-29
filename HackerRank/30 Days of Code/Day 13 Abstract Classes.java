baad64dbb76fee47900fb9789017942ba34f54d0
null-kryptonian <57069206+null-kryptonian@users.noreply.github.com>
2020-07-29 22:06:09 +0600

    Create Day 13 Abstract Classes.java

---
+++ HackerRank/30 Days of Code/Day 13 Abstract Classes.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+abstract class Book {
+    String title;
+    String author;
+    
+    Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+    
+    abstract void display();
+}
+
+// Declare your class here. Do not use the 'public' access modifier.
+    class MyBook extends Book {
+    int price;
+    
+    MyBook(String title, String author, int price) {
+        super(title, author);
+        this.price = price;
+    }
+    
+    void display() {
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("Price: " + price);
+    }
+}
+    
+// End class
+
+public class Solution {
+   
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String title = scanner.nextLine();
+        String author = scanner.nextLine();
+        int price = scanner.nextInt();
+        scanner.close();
+
+        Book book = new MyBook(title, author, price);
+        book.display();
+    }
+}
