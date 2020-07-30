fc23c1981020ce77c2656f259e2d253d4737874e
null-kryptonian <57069206+null-kryptonian@users.noreply.github.com>
2020-07-30 21:32:08 +0600

    Create Functions in C.c

---
+++ HackerRank/C/Functions in C.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int max_of_four(int a, int b, int c, int d)
+{
+    int ans = a;
+    if(ans < b){
+        ans = b;
+    }
+    if(ans < c){
+        ans = c;
+    }
+    if(ans < d){
+        ans = d;
+    }
+    return ans;
+}
+
+
+int main() {
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int ans = max_of_four(a, b, c, d);
+    printf("%d", ans);
+    
+    return 0;
+}
+
