c0b8769409ba8dd4c40e19c08207d34af12e780a
null-kryptonian <57069206+null-kryptonian@users.noreply.github.com>
2020-07-30 21:33:44 +0600

    Create For Loop in C.c

---
+++ HackerRank/C/For Loop in C.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+
+
+int main() 
+{
+    int a, b;
+    scanf("%d\n%d", &a, &b);
+    const char *numbers[9] = {"one", "two", "three", "four", "five", "six", "seven",                                     "eight", "nine"};
+  	for(int i = a; i <= b; ++i){
+        if(i > 0 && i < 10){
+            printf("%s\n", numbers[i - 1]);
+        }
+        else{
+            printf("%s\n", (i%2) ? "odd" : "even");
+        }
+    }
+    return 0;
+}
