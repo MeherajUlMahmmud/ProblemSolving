975588048ec3c35c292e56e11ce7cfb9d71a44ea
null-kryptonian <57069206+null-kryptonian@users.noreply.github.com>
2020-07-30 21:40:11 +0600

    Create Basic Data Types.cpp

---
+++ HackerRank/C++/Basic Data Types.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int a;
+    long long int b;
+    char c;
+    float d;
+    double e;
+    scanf("%d %lld %c %f %lf", &a, &b, &c, &d, &e);
+    printf("%d\n%lld\n%c\n%.3f\n%.9lf", a, b, c, d, e);
+    return 0;
+}
