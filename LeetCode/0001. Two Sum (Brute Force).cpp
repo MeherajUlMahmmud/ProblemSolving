266ea468eb89ee1d806b675875a56d88cac2bd69
null-kryptonian <57069206+null-kryptonian@users.noreply.github.com>
2020-08-07 21:03:24 +0600

    Create 0001. Two Sum (Brute Force).cpp

---
+++ LeetCode/0001. Two Sum (Brute Force).cpp
@@ -0,0 +1,25 @@
+class Solution
+{
+public:
+    vector<int> twoSum(vector<int>& nums, int target)
+    {
+        vector<int> arr{};
+        int n = nums.size();
+        for(int i = 0;  i < n; i++)
+        {
+            for(int j = 0; j < n; j++)
+            {
+                if(i != j)
+                {
+                    if(nums[i] + nums[j] == target)
+                    {
+                        arr.push_back(i);
+                        arr.push_back(j);
+                        return arr;
+                    }
+                }
+            }
+        }
+        return arr;
+    }
+};
