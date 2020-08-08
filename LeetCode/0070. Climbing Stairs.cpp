26f3650dfbcb2d0a6ebea16a8e549baaba17da34
null-kryptonian <57069206+null-kryptonian@users.noreply.github.com>
2020-08-08 15:37:41 +0600

    Create 0070. Climbing Stairs.cpp

---
+++ LeetCode/0070. Climbing Stairs.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        int steps[n+1];
+        steps[0] = 1;
+        steps[1] = 1;
+        for(int i = 2; i <= n; i++) {
+            steps[i] = steps[i-1] + steps[i-2];
+        }
+        return steps[n];
+    }
+};
