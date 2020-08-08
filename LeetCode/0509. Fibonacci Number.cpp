0f4704155f225d17a5928fcbc2be3040cfd46795
null-kryptonian <57069206+null-kryptonian@users.noreply.github.com>
2020-08-08 15:37:46 +0600

    Create 0509. Fibonacci Number.cpp

---
+++ LeetCode/0509. Fibonacci Number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int fib(int N) {
+        if (N == 0)
+            return 0;
+        if(N == 1)
+            return 1;
+        
+        int arr[N+1];
+        arr[0] = 0;
+        arr[1] = 1;
+        for(int i = 2; i <= N;i++) {
+            arr[i] = arr[i-1] + arr[i-2];
+        }
+        return arr[N];
+    }
+};
