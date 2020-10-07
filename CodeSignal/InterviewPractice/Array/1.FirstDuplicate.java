c8a1b7ce4d1e25884cc37975cc80f23ac865d564
null-kryptonian <57069206+null-kryptonian@users.noreply.github.com>
2020-10-07 18:50:45 +0600

    Update 1.FirstDuplicate.java

--- CodeSignal/InterviewPractice/Array/1.FirstDuplicate.java
+++ CodeSignal/InterviewPractice/Array/1.FirstDuplicate.java
@@ -1,3 +1,4 @@
+// using mathematical expression (only valid for positive integers)
 int firstDuplicate(int[] a) {
     for(int i = 0; i < a.length; i++) {
         if(a[Math.abs(a[i]) - 1] < 0) {
@@ -9,3 +10,15 @@ int firstDuplicate(int[] a) {
     }
     return -1;
 }
+
+// using set (set only contains distinct elements)
+int firstDuplicate(int[] a) {
+    Set<Integer> set = new HashSet<>();
+    for(int i = 0; i < a.length; i++) {
+        if(!set.add(a[i])) {
+            return a[i];
+        }
+    }
+    return -1;
+}
+
