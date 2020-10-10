bb7d1cf92f11ce1342a2f52b347c1a251458e1ba
null-kryptonian <57069206+null-kryptonian@users.noreply.github.com>
2020-10-10 21:23:45 +0600

    Create 0104. Maximum Depth of Binary Tree.cpp

---
+++ LeetCode/0104. Maximum Depth of Binary Tree.cpp
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        //if(!root) return 0;
+        
+        // int left = maxDepth(root->left);
+        // int right = maxDepth(root->right);
+        
+        //return max(left, right) + 1;
+        // return max(maxDepth(root->left), maxDepth(root->right)) + 1;
+        return !root ? 0 : max(maxDepth(root->left), maxDepth(root->right)) + 1;
+    }
+};
