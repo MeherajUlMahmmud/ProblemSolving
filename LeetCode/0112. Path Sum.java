e2a359212337e6fdced0dc7c9a19a696c070e308
Meheraj <57069206+superior-prog@users.noreply.github.com>
2021-02-26 21:54:59 +0600

    Create 0112. Path Sum.java

---
+++ LeetCode/0112. Path Sum.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null) return false;
+        
+        Stack<TreeNode> node_stack = new Stack();
+        Stack<Integer> sum_stack = new Stack();
+        
+        node_stack.add(root);
+        sum_stack.add(sum - root.val);
+        
+        while(!node_stack.isEmpty()) {
+            TreeNode cur_node = node_stack.pop();
+            int cur_sum = sum_stack.pop();
+            
+            if(cur_node.left == null && cur_node.right == null && cur_sum == 0)
+                return true;
+            
+            if(cur_node.left != null) {
+                node_stack.add(cur_node.left);
+                sum_stack.add(cur_sum - cur_node.left.val);
+            }
+            
+            if(cur_node.right != null) {
+                node_stack.add(cur_node.right);
+                sum_stack.add(cur_sum - cur_node.right.val);
+            }
+        }
+        return false;
+    }
+}
