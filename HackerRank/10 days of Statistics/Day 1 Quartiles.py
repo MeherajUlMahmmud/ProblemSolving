a6ebcd11336d68fd062ec456bdfad65577ef2244
Meheraj <57069206+superior-prog@users.noreply.github.com>
2021-03-01 22:58:05 +0600

    Update Day 1 Quartiles.py

--- HackerRank/10 days of Statistics/Day 1 Quartiles.py
+++ HackerRank/10 days of Statistics/Day 1 Quartiles.py
@@ -1,18 +1,18 @@
 def median(nums):
-    if len(nums)%2 == 0:
-        return int(sum(nums[len(nums)//2-1:len(nums)//2+1])/2)
+    if len(nums) % 2 == 0:
+        return int(sum(nums[len(nums)//2 - 1 : len(nums)//2 + 1]) / 2)
     else:
-        return nums[len(nums)//2]
+        return nums[len(nums) // 2]
 
 
 def quartiles(N,nums):
-    Q1 = median(nums[:len(nums)//2])
+    Q1 = median(nums[: len(nums) // 2])
     Q2 = median(nums)
     if N%2 == 0:
-        Q3 = median(nums[len(nums)//2:])
+        Q3 = median(nums[len(nums) // 2 :])
     else:
-        Q3 = median(nums[len(nums)//2+1:])
-    return Q1,Q2,Q3
+        Q3 = median(nums[len(nums)//2 + 1 :])
+    return Q1, Q2, Q3
 
 
 
@@ -20,7 +20,7 @@ n = int(input())
 data = [int(val) for val in input().split()]
 data.sort()
 
-Q1,Q2,Q3 = quartiles(n, data)
+Q1, Q2, Q3 = quartiles(n, data)
 print(Q1)
 print(Q2)
 print(Q3)
