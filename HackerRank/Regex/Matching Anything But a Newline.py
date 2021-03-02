a7204071bda369ba8ef967e1e177d7df5c92d8b1
Meheraj <57069206+superior-prog@users.noreply.github.com>
2021-03-02 14:13:39 +0600

    Update Matching Anything But a Newline.py

--- HackerRank/Regex/Matching Anything But a Newline.py
+++ HackerRank/Regex/Matching Anything But a Newline.py
@@ -8,7 +8,8 @@ regex_pattern = r"...\....\....\...."	# Do not delete 'r'.
 # We use . to match "anycharacter" in regex. It matches everything but things like line breaks.
 # And \. matches actual periods in the string.
 # We have to escape the . because it is a metacharacter.
-# So, in the solution, we're saying that between the beginning of the string ^ and the end of the string $ there will be exactly 4 groups of 3 "anycharacters" delimited by periods
+# So, in the solution, we're saying that between the beginning of the string ^
+# and the end of the string $ there will be exactly 4 groups of 3 "anycharacters" delimited by periods
 
 import re
 import sys
