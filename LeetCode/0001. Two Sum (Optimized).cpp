c2309b804d7fec34c39ea1639db151e7547ec335
Meheraj <meherajmahmmd@gmail.com>
2021-11-14 14:42:36 +0600

    Update 0001. Two Sum (Optimized).cpp

--- LeetCode/0001. Two Sum (Optimized).cpp
+++ LeetCode/0001. Two Sum (Optimized).cpp
@@ -1,44 +1,26 @@
-class Solution
-{
+class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target)
-    {
-        vector<int> arr;
-        // PAIR is an STL data structure
-        // it is more like an struct
-        /*
-        struct pair {
-            int first;
-            int second;
-        };
-        */
-        vector<pair<int, int>> nums2;
-        
+    vector<int> twoSum(vector<int>& nums, int target) {
         int n = nums.size();
+        int i = 0, j = n-1;
         
-        // copying elements and indices of nums into nums2
-        for(int i = 0; i < n; i++) {
+        vector<pair<int, int>> nums2;
+        for (int i = 0; i < n; i++) {
             pair<int, int> temp(nums[i], i);
             nums2.push_back(temp);
         }
         
-        // sorting nums2 in ascending order
         sort(nums2.begin(), nums2.end());
         
-        int i = 0, j = nums2.size()-1;
         while(i < j) {
             if(nums2[i].first + nums2[j].first == target) {
-                arr.push_back(nums2[i].second);
-                arr.push_back(nums2[j].second);
-                return arr;
-            }
-            else if(nums2[i].first + nums2[j].first > target) {
+                return {nums2[i].second, nums2[j].second};
+            } else if(nums2[i].first + nums2[j].first > target) {
                 j--;
-            }
-            else {
+            } else {
                 i++;
             }
         }
-        return arr;
+        return {-1, -1};
     }
 };
