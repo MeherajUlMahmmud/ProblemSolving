9cf4d7de75a0303ded68dcdb48221d80e746d948
Meheraj <meherajmahmmd@gmail.com>
2021-11-14 14:42:26 +0600

    Update 0053. Maximum Subarray.cpp

--- LeetCode/0053. Maximum Subarray.cpp
+++ LeetCode/0053. Maximum Subarray.cpp
@@ -1,12 +1,15 @@
-class Solution {
+class Solution
+{
 public:
-    int maxSubArray(vector<int>& nums) {
-        int maxSum = nums[0], curSum = 0;
-        for(int i = 0; i < nums.size(); i++) {
-            if(curSum < 0)
+    int maxSubArray(vector<int> &nums)
+    {
+        int maxSum = nums[0], curSum = nums[0];
+        for (int i = 1; i < nums.size(); i++)
+        {
+            if (curSum < 0)
                 curSum = 0;
-            curSum += i;
-            maxSum = max(maxSum, curSum);
+            curSum += nums[i];
+            maxSum = max(curSum, maxSum);
         }
         return maxSum;
     }
