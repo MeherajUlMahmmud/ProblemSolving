0590e41cadcb74be36a9dabd478c51962bbec7ee
Meheraj <meherajmahmmd@gmail.com>
2021-11-14 14:42:16 +0600

    Update 0088. Merge Sorted Array.cpp

--- LeetCode/0088. Merge Sorted Array.cpp
+++ LeetCode/0088. Merge Sorted Array.cpp
@@ -1,10 +1,13 @@
-class Solution {
+class Solution
+{
 public:
-    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+    void merge(vector<int> &nums1, int m, vector<int> &nums2, int n)
+    {
         int j = 0;
-        for(int i = m; i < nums1.size(); ++i) {
+        for (int i = m; i < nums1.size(); i++)
+        {
             nums1[i] = nums2[j];
-            ++j;
+            j++;
         }
         sort(nums1.begin(), nums1.end());
     }
