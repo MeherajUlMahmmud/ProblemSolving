b3148a4c4dd48d627a46dac6933a1573b0966035
Meheraj <57069206+MeherajUlMahmmud@users.noreply.github.com>
2024-01-02 21:32:37 +0600

    Update 0112. Path Sum.java

--- LeetCode/0112. Path Sum.java
+++ LeetCode/0112. Path Sum.java
@@ -1,41 +1,55 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class Solution {
     public boolean hasPathSum(TreeNode root, int sum) {
-        if (root == null) return false;
-        
+        // if (root == null) return false;
+
+        // if (root.left == null && root.right == null)
+        // return sum - root.val == 0;
+
+        // if (root.left != null && hasPathSum(root.left, sum - root.val))
+        // return true;
+
+        // if (root.right != null && hasPathSum(root.right, sum - root.val))
+        // return true;
+
+        // return false;
+
+        if (root == null)
+            return false;
+
         Stack<TreeNode> node_stack = new Stack();
         Stack<Integer> sum_stack = new Stack();
-        
+
         node_stack.add(root);
         sum_stack.add(sum - root.val);
-        
-        while(!node_stack.isEmpty()) {
+
+        while (!node_stack.isEmpty()) {
             TreeNode cur_node = node_stack.pop();
             int cur_sum = sum_stack.pop();
-            
-            if(cur_node.left == null && cur_node.right == null && cur_sum == 0)
+
+            if (cur_node.left == null && cur_node.right == null && cur_sum == 0)
                 return true;
-            
-            if(cur_node.left != null) {
+
+            if (cur_node.left != null) {
                 node_stack.add(cur_node.left);
                 sum_stack.add(cur_sum - cur_node.left.val);
             }
-            
-            if(cur_node.right != null) {
+
+            if (cur_node.right != null) {
                 node_stack.add(cur_node.right);
                 sum_stack.add(cur_sum - cur_node.right.val);
             }
